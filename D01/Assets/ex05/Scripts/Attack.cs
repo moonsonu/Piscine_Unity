750894295f45e6c5aebfa93fd9a8a44f08165949
Kristine Sonu <moonyoning@Kristines-MacBook-Pro.local>
2019-02-09 05:16:33 -0800

    D01ex05

---
+++ D01/Assets/ex05/Scripts/Attack.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Attack : MonoBehaviour
+{
+    public Turret turretAI;
+
+    void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Thomas")
+            turretAI.Attack(true);
+    }
+}
---
+++ D01/Assets/ex05/Scripts/Turret.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Turret : MonoBehaviour
+{
+    public float distance;
+    public float wakeRange;
+    public float shootInterval;
+    public float bulletSpeed = 10;
+    public float bulletTimer;
+
+    public bool attack;
+
+    public GameObject bullet;
+    public Transform target;
+    public Transform shootpoint;
+
+    void Update()
+    {
+        RangeCheck();
+    }
+
+    void RangeCheck()
+    {
+        distance = Vector3.Distance(transform.position, target.transform.position);
+        if (distance < wakeRange)
+            attack = true;
+        else
+            attack = false;
+    }
+
+    public void Attack(bool attack)
+    {
+
+        bulletTimer += Time.deltaTime;
+        Debug.Log(bulletTimer);
+        if (bulletTimer <= shootInterval)
+        {
+            if (attack)
+            {
+                Vector2 direction = target.transform.position - transform.position;
+                direction.Normalize();
+                GameObject bulletClone;
+                bulletClone = Instantiate(bullet, shootpoint.transform.position, shootpoint.transform.rotation) as GameObject;
+                bulletClone.GetComponent<Rigidbody2D>().velocity = shootpoint.transform.rotation * direction * bulletSpeed;
+                bulletTimer = 0;
+            }
+        }
+    }
+}
