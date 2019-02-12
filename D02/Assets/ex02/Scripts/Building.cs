11b614045dc47b63e1e2f5aea32d627a590487fc
Kristine Sonu <moonk90@gmail.com>
2019-02-11 20:00:38 -0800

    D02ex02

---
+++ D02/Assets/ex02/Scripts/Building.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Building : MonoBehaviour
+{
+    public int HP;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        
+    }
+}
---
+++ D02/Assets/ex02/Scripts/TownHall.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TownHall : MonoBehaviour
+{
+    public GameObject Spawn;
+    public Vector3 position;
+    public float time = 0.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        time += Time.deltaTime;
+        if (time > 10)
+        {
+            CharacterSpawn();
+            time = 0;
+        }
+    }
+
+    void CharacterSpawn()
+    {
+        GameObject tmp = Instantiate(Spawn, position, Quaternion.identity) as GameObject;
+    }
+}
