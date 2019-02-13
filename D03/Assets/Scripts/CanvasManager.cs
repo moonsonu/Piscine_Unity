bd237356fbc3f2f3a0e0894d302eeb28b739c680
Kristine Sonu <moonk90@gmail.com>
2019-02-13 15:09:44 -0800

    D03ex01inProgess

---
+++ D03/Assets/Scripts/CanvasManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CanvasManager : MonoBehaviour
+{
+    public gameManager gm;
+    public Text hpLabel;
+    public Text energyLabel;
+
+    void Start()
+    {
+        hpLabel.text = "20";
+        energyLabel.text = "300";
+    }
+
+    void Update()
+    {
+        int hp = CalcHP();
+        int energy = CalcEnergy();
+        hpLabel.text = "" + hp;
+        energyLabel.text = "" + energy;
+    }
+
+    int CalcHP()
+    {
+        return (int)gm.playerHp;
+    }
+
+    int CalcEnergy()
+    {
+        return (int)gm.playerEnergy;
+    }
+}
