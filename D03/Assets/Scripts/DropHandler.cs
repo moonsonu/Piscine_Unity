3491add1c18b56181661666dc93ac0b95a731f73
Kristine Sonu <moonk90@gmail.com>
2019-02-13 11:28:56 -0800

    D03ex01inProgress

---
+++ D03/Assets/Scripts/DropHandler.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+
+public class DropHandler : MonoBehaviour, IDropHandler
+{
+    public void OnDrop(PointerEventData eventData)
+    {
+        RectTransform item = transform as RectTransform;
+        if (!RectTransformUtility.RectangleContainsScreenPoint(item, Input.mousePosition))
+        {
+            Debug.Log("Drop Item");
+        }
+    }
+}
