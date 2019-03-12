b6065569e71417f44cd9f6386e29aae3b7ddcfb2
Kristine Sonu <ksonu@e1z2r3p9.42.us.org>
2019-03-11 22:13:32 -0700

    ex01

---
+++ D05/Assets/ex01/Scripts/GUIController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GUIController : MonoBehaviour
+{
+    public Rect position = new Rect(200, 15, 150, 25);
+    public string text = "Hello World";
+    public GUISkin skin = null;
+
+    private void OnGUI()
+    {
+        GUI.skin = skin;
+        GUI.Label(position, text);
+    }
+}
