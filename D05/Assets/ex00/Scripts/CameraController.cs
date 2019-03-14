1858868da0502f47a2aa824d209153d23bf739e8
Kristine Sonu <ksonu@e1z2r3p9.42.us.org>
2019-03-13 20:07:06 -0700

    camera

--- D05/Assets/ex00/Scripts/CameraController.cs
+++ D05/Assets/ex00/Scripts/CameraController.cs
@@ -12,29 +12,44 @@ public class CameraController : MonoBehaviour
 
     void Start()
     {
-        
+        Debug.Log("Width" + Screen.width + "height" + Screen.height);
     }
 
     void Update()
     {
         Vector3 pos = transform.position;
         if (Input.GetKey("w"))
-            pos.z += speedKey * Time.deltaTime;
+        {
+            if (pos.z <= Screen.width)
+                pos.z += speedKey * Time.deltaTime;
+        }
 
         if (Input.GetKey("s"))
             pos.z -= speedKey * Time.deltaTime;
 
         if (Input.GetKey("a"))
-            pos.x -= speedKey * Time.deltaTime;
+        {
+            if (pos.x >= 50)
+                pos.x -= speedKey * Time.deltaTime;
+        }
 
         if (Input.GetKey("d"))
-            pos.x += speedKey * Time.deltaTime;
+        {
+            if (pos.x <= Screen.width + 50)
+                pos.x += speedKey * Time.deltaTime;
+        }
 
         if (Input.GetKey("e"))
-            pos.y += speedKey * Time.deltaTime;
+        {
+            if (pos.y <= Screen.height)
+                pos.y += speedKey * Time.deltaTime;
+        }
 
         if (Input.GetKey("q"))
-            pos.y -= speedKey * Time.deltaTime;
+        {
+            if (pos.y >= 0)
+                pos.y -= speedKey * Time.deltaTime;
+        }
         mouseX -= speedMouse * Input.GetAxis("Mouse Y");
         mouseY += speedMouse * Input.GetAxis("Mouse X");
         transform.eulerAngles = new Vector3(mouseX, mouseY, 0);
