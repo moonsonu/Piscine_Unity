bee92ece838702a6252fbad6940074f89cec0b05
Kristine Sonu <moonk90@gmail.com>
2019-03-15 05:12:32 -0700

    D06

---
+++ D06/Assets/Scripts/CameraController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public enum RotationAxis
+    {
+        mouseX = 1,
+        mouseY = 2
+    }
+
+    public RotationAxis axes = RotationAxis.mouseX;
+    public float minVert = -45f;
+    public float maxVert = 45f;
+    public float Horiz = 10f;
+    public float Vert = 10f;
+    public float rotateX = 0;
+
+    void Update()
+    {
+        if (axes == RotationAxis.mouseX)
+            transform.Rotate(0, Input.GetAxis("Mouse X") * Horiz, 0);
+        else if (axes == RotationAxis.mouseY)
+        {
+            rotateX -= Input.GetAxis("Mouse Y") * Vert;
+            rotateX = Mathf.Clamp(rotateX, minVert, maxVert);
+            float rotateY = transform.localEulerAngles.y;
+            rotateY = Mathf.Clamp(rotateY, minVert, maxVert);
+            transform.localEulerAngles = new Vector3(rotateX, rotateY, 0);
+        }
+    }
+}
---
+++ D06/Assets/Scripts/MovementController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementController : MonoBehaviour
+{
+    public float speed = 6f;
+    //public float gravity = -9.8f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float deltaX = Input.GetAxis("Horizontal") * speed;
+        float deltaZ = Input.GetAxis("Vertical") * speed;
+        Vector3 movement = new Vector3(deltaX, 0, deltaZ);
+        //movement = Vector3.ClampMagnitude(movement, speed); //limits the max speed of the player
+        ////movement.y = gravity;
+        //movement *= Time.deltaTime;
+        ////movement = transform.TransformDirection(movement);
+        //transform.position += movement;
+        transform.position += movement * Time.deltaTime * speed;
+    }
+}
